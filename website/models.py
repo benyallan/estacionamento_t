3621ed52f90bebe87f9e1df1791e6cb74baa71e5
Beny Allan <benyallan@yahoo.com.br>
2022-02-05 16:02:43 -0300

    finalizado

--- website/models.py
+++ website/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Contato(models.Model):
+
+    nome = models.CharField(max_length=100)
+    sobrenome = models.CharField(max_length=100)
+    email = models.EmailField("e-mail", max_length=254)
+    endereco = models.CharField("endereço", max_length=100)
+    mensagem = models.TextField()
+    noticias = models.BooleanField("notícias")
+
+    class Meta:
+        verbose_name = ("contato")
+        verbose_name_plural = ("contatos")
+
+    def __str__(self):
+        return self.nome
+
+
--- website/views.py
+++ website/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Contato
 
 # Create your views here.
 def home(request):
@@ -6,7 +7,24 @@ def home(request):
 
 
 def contato(request):
-    return render(request, 'website/contato.html')
+    mensagem = ''
+    if request.method == 'POST':
+        try:
+            contato = {}
+            contato['email'] = request.POST.get('email')
+            contato['nome'] = request.POST.get('nome')
+            contato['sobrenome'] = request.POST.get('sobrenome')
+            contato['endereco'] = request.POST.get('endereco')
+            contato['mensagem'] = request.POST.get('mensagem')
+            contato['noticias'] = True if request.POST.get('noticias') == 'on' else False
+            
+            Contato.objects.create(**contato)
+        except Exception as e:
+            mensagem = str(e)
+        else:
+            mensagem = "Contato realizado com sucesso"
+    
+    return render(request, 'website/contato.html', {'mensagem': mensagem})
 
 
 def servicos(request):
